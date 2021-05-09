3aa0dd13cd8ffc127af983cd5b6bbb141341ea8b
AppCrashExpress <efimov.s2000@gmail.com>
2021-05-09 18:47:42 +0300

    Switch LU decomposition to numpy

--- lab-1/lu-decompose/lu_decompose.py
+++ lab-1/lu-decompose/lu_decompose.py
@@ -1,25 +1,25 @@
-from matrix  import create_matrix, copy_matrix, check_square, str_matrix
-from math    import isclose
+import numpy as np
 import logging, json
+from math import isclose
 
 def decompose_matrix(matrix):
     """Decomposes the matrix into lower and upper matrices"""
-    if not check_square(matrix):
+    if matrix.shape[0] != matrix.shape[1]:
         raise IndexError("Recieved matrix is not square")
     
     matrix_size = len(matrix)
-    lower = create_matrix(matrix_size)
-    upper = copy_matrix(matrix)
+    lower = np.zeros_like(matrix, dtype=np.float32)
+    upper = np.array(matrix, dtype=np.float32)
 
-    permut = [i for i in range(matrix_size + 1)]
+    permut = np.fromfunction(lambda i : i, (matrix_size + 1, ), dtype=np.int16)
     permut[matrix_size] = 0
 
     for iter_i in range(matrix_size):
         max_i = iter_i
-        max_val = abs(matrix[iter_i][iter_i])
+        max_val = abs(matrix[iter_i, iter_i])
 
         for i in range(iter_i + 1, matrix_size):
-            abs_val = abs(matrix[i][iter_i])
+            abs_val = abs(matrix[i, iter_i])
             if abs_val > max_val:
                 max_val = abs_val
                 max_i = i
@@ -28,17 +28,15 @@ def decompose_matrix(matrix):
             raise RuntimeError("Recieved matrix is degenerate")
 
         if iter_i != max_i:
-            permut[max_i], permut[iter_i] = permut[iter_i], permut[max_i]
-            upper[max_i], upper[iter_i] = upper[iter_i], upper[max_i]
-            lower[max_i], lower[iter_i] = lower[iter_i], lower[max_i]
+            permut[[max_i, iter_i]] = permut[[iter_i, max_i]]
+            upper[[max_i, iter_i]] = upper[[iter_i, max_i]]
+            lower[[max_i, iter_i]] = lower[[iter_i, max_i]]
             permut[matrix_size] += 1
 
-        lower[iter_i][iter_i] = 1
-        for coef_i in range(iter_i + 1, matrix_size):
-            lower[coef_i][iter_i] = upper[coef_i][iter_i] / upper[iter_i][iter_i]
-
-        for row_i in range(iter_i + 1, matrix_size):
-            upper[row_i] = [x - y * lower[row_i][iter_i] for (x, y) in zip(upper[row_i], upper[iter_i])]
+        lower[iter_i, iter_i] = 1
+        lower[iter_i+1:, iter_i] = (upper[iter_i+1:, iter_i] / upper[iter_i, iter_i])
+        
+        upper[iter_i+1:] = upper[iter_i+1:] - np.outer(lower[iter_i+1:, iter_i], upper[iter_i])
 
     return (lower, upper, permut)
 
@@ -47,15 +45,15 @@ def solve_lu(lower, upper, permutation, constants):
     (equation constraints)"""
     matrix_size = len(lower)
 
-    constants = [constants[p] for p in permutation[:-1]]
+    constants = constants[permutation[:-1]]
 
-    temp_constants = [0] * matrix_size
+    temp_constants = np.zeros_like(constants, dtype=np.float32)
     for i in range(matrix_size):
         for j in range(i):
             temp_constants[i] -= temp_constants[j] * lower[i][j]
         temp_constants[i] = (temp_constants[i] + constants[i]) / lower[i][i]
 
-    result = [0] * matrix_size
+    result = np.zeros_like(constants, dtype=np.float32)
     for i in reversed(range(matrix_size)):
         for j in range(i + 1, matrix_size):
             result[i] -= result[j] * upper[i][j]
@@ -80,20 +78,15 @@ def calc_lu_determinant(lower, upper, p):
 def calc_lu_inverse(lower, upper, p):
     """Calculates columns of matrice inverse from its decomposition.
     Transposition ensures the same direction of lists as original"""
-    def transpose(matrix):
-        matrix_size = len(matrix)
-        for i in range(matrix_size):
-            for j in range(i + 1, matrix_size):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
     matrix_size = len(lower)
-    result = [0] * matrix_size
+    result = np.zeros_like(lower, dtype=np.float32)
     for i in range(matrix_size):
-        inverse_column = [0] * i + [1] + [0] * (matrix_size - 1 - i)
+        inverse_column = np.zeros((matrix_size, ))
+        inverse_column[i] = 1
         result[i] = solve_lu(lower, upper, p, inverse_column)
 
-    transpose(result)
-    return result
+    return result.T
+
 
 if __name__ == "__main__":
     logging.basicConfig(filename='lu-decomp.log', encoding='utf-8', level=logging.INFO)
@@ -101,17 +94,17 @@ if __name__ == "__main__":
     with open("task.json", "r") as json_file:
         task = json.load(json_file)
 
-    matrix = task["matrix"]
-    constants = task["constants"]
+    matrix = np.array(task["matrix"])
+    constants = np.array(task["constants"])
 
-    logging.info("Input matrix:\n%s", str_matrix(matrix))
+    logging.info("Input matrix:\n%s", str(matrix))
     logging.info("Matrix constants: %s", str(constants))
 
     decomposition = decompose_matrix(matrix)
 
-    logging.info("Lower matrix:\n%s", str_matrix(decomposition[0]))
-    logging.info("Upper matrix:\n%s", str_matrix(decomposition[1]))
-    logging.info("Permutation array: %s", decomposition[2])
+    logging.info("Lower matrix:\n%s", str(decomposition[0]))
+    logging.info("Upper matrix:\n%s", str(decomposition[1]))
+    logging.info("Permutation array: %s", str(decomposition[2]))
 
     res = solve_lu(*decomposition, constants)
 
@@ -121,7 +114,7 @@ if __name__ == "__main__":
     inv = calc_lu_inverse(*decomposition)
 
     logging.info("Determinant: %i", det)
-    logging.info("Matrix inverse:\n%s", str_matrix(inv))
+    logging.info("Matrix inverse:\n%s", str(inv))
 
     print("Solution: ", res)
     
