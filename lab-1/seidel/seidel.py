3662c393cb53d35b4ae28f1807d330eb3330c256
AppCrashExpress <efimov.s2000@gmail.com>
2021-05-09 21:11:09 +0300

    Move seidel to numpy

--- lab-1/seidel/seidel.py
+++ lab-1/seidel/seidel.py
@@ -1,47 +1,30 @@
-from matrix import create_matrix, str_matrix
-from math import sqrt
+import numpy as np
 import logging, json
+from math import sqrt
+from numpy.linalg import norm
 
 MAX_ITER_COUNT = 1000
 
 def calc_equivalent(matrix, contants):
-    new_matrix = create_matrix(len(matrix))
-    new_constants = [0] * len(constants)
-    
-    for i in range(len(matrix)):
-        diag_val = matrix[i][i]
-        if diag_val == 0:
+    new_matrix = np.zeros_like(matrix)
+    new_constants = np.zeros_like(constants)
+
+    diag = matrix.diagonal()
+
+    if not(all(diag != 0)):
             raise ValueError("Diagonal values can not contain 0")
 
-        for j in range(len(matrix[0])):
-            if i == j:
-                new_matrix[i][j] = 0
-            else:
-                new_matrix[i][j] = - matrix[i][j] / diag_val
-
-        new_constants[i] = constants[i] / diag_val
-
-    return (new_matrix, new_constants)
+    new_matrix = - matrix / diag[:, np.newaxis]
+    for i in range(len(new_matrix)):
+        new_matrix[i, i] = 0
+    new_constants = constants / diag
     
+    return (new_matrix, new_constants)
 
-def calc_vec_norm(x):
-    vec_sum = 0
-    for val in x:
-        vec_sum += val * val
-
-    return sqrt(vec_sum)
-
-def calc_matr_norm(x):
-    matr_sum = 0
-    for row in x:
-        for val in row:
-            matr_sum += val * val
-
-    return sqrt(matr_sum)
 
 def test_exit(prev_res, new_res, coef_divident, coef_divisor, eps):
-    difference = [x - x0 for x, x0 in zip (new_res, prev_res)]
-    diff_norm = calc_vec_norm(difference)
+    difference = new_res - prev_res
+    diff_norm = norm(difference)
 
     if coef_divisor < 1:
         return diff_norm * coef_divident / (1 - coef_divisor) <= eps
@@ -49,49 +32,37 @@ def test_exit(prev_res, new_res, coef_divident, coef_divisor, eps):
         return diff_norm <= eps
 
 def solve_iter(matrix, constants, eps = 10e-5):
-    matrix_norm = calc_matr_norm(matrix)
-    x_values = constants[:]
-    new_x_values = constants[:]
+    matrix_norm  = norm(matrix)
+    x_values     = constants.copy()
+    new_x_values = constants.copy()
     
     for iter_no in range(MAX_ITER_COUNT):
-        for i in range(len(matrix)):
-            row_sum = sum([a * x for a, x in zip(matrix[i], x_values)])
-            new_x_values[i] = constants[i] + row_sum
+        row_sums = np.sum(matrix * x_values, axis=1)
+        new_x_values = constants + row_sums
 
         if test_exit(x_values, new_x_values, matrix_norm, matrix_norm, eps):
             break
         else:
-            x_values = new_x_values[:]
+            x_values = new_x_values.copy()
 
     return new_x_values
 
 def solve_seidel(matrix, constants, eps = 10e-5):
-    def get_upper(matrix):
-        upper = create_matrix(len(matrix))
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                if j > i:
-                    continue
-                else:
-                    upper[i][j] = matrix[i][j]
-
-        return upper
-
-    upper        = get_upper(matrix)
-    upper_norm   = calc_matr_norm(upper)
-    matrix_norm  = calc_matr_norm(matrix)
-    x_values     = constants[:]
-    new_x_values = constants[:]
+    upper        = np.triu(matrix)
+    upper_norm   = norm(upper)
+    matrix_norm  = norm(matrix)
+    x_values     = constants.copy()
+    new_x_values = constants.copy()
     
     for iter_no in range(MAX_ITER_COUNT):
         for i in range(len(matrix)):
-            row_sum = sum([a * x for a, x in zip(matrix[i], new_x_values)])
+            row_sum = sum(matrix[i] * new_x_values)
             new_x_values[i] = constants[i] + row_sum
 
         if test_exit(x_values, new_x_values, upper_norm, matrix_norm, eps):
             break
         else:
-            x_values = new_x_values[:]
+            x_values = new_x_values.copy()
 
     return new_x_values
 
@@ -101,20 +72,20 @@ if __name__ == "__main__":
     with open("task.json", "r") as json_file:
         task = json.load(json_file)
 
-    matrix = task["matrix"]
-    constants = task["constants"]
+    matrix = np.array(task["matrix"])
+    constants = np.array(task["constants"])
     try:
         eps = task["epsilon"]
     except KeyError:
         eps = 10e-5
 
-    logging.info("Input matrix:\n%s", str_matrix(matrix))
+    logging.info("Input matrix:\n%s", str(matrix))
     logging.info("Matrix constants: %s", str(constants))
     logging.info("Selected epsilon: %f", eps)
 
     new_a, new_b = calc_equivalent(matrix, constants)
 
-    logging.info("Equivalent matrix:\n%s", str_matrix(new_a))
+    logging.info("Equivalent matrix:\n%s", str(new_a))
     logging.info("Equivalent constants: %s", str(new_b))
 
     res_i = solve_iter(new_a, new_b, eps)
