6353d91020e900da53cde85341f7ca71a5aab8d0
AppCrashExpress <efimov.s2000@gmail.com>
2021-05-11 19:22:52 +0300

    Add newton interpolation method

--- lab3/interpolation/interp.py
+++ lab3/interpolation/interp.py
@@ -22,6 +22,36 @@ def lagrange_interpolate(x, pivots):
 
     return y
 
+def newton_interpolate(x, pivots):
+    def build_diffs(xs, diffs, i, j):
+        if i == j:
+            return diffs[i][j]
+
+        if diffs[i][j - 1] is None:
+            build_diffs(xs, diffs, i, j - 1)
+        if diffs[i + 1][j] is None:
+            build_diffs(xs, diffs, i + 1, j)
+
+        diffs[i][j] = (diffs[i + 1][j] - diffs[i][j - 1]) / (xs[j] - xs[i])
+
+    pivots.sort()
+
+    xs, ys = [list(t) for t in zip(*pivots)]
+
+    div_diffs = [[None for _ in pivots] for _ in pivots]
+    for i in range(len(pivots)):
+        div_diffs[i][i] = ys[i]
+    build_diffs(xs, div_diffs, 0, len(pivots) - 1)
+
+    y = 0
+    for i in range(len(pivots)):
+        coef = 1
+        for j in range(i):
+            coef *= x - xs[j]
+        y += div_diffs[0][i] * coef
+
+    return y
+
 def main():
     f = lambda x : sqrt(x)
     xs = [0, 1.7, 3.4, 5.1]
@@ -29,10 +59,12 @@ def main():
     points = list(map(lambda x : (x, f(x)), xs))
 
     real_x = f(3)
-    interp_x = lagrange_interpolate(3, points)
+    lagrange_x = lagrange_interpolate(3, points)
+    newton_x = newton_interpolate(3, points)
 
     print(real_x)
-    print(interp_x)
+    print(lagrange_x)
+    print(newton_x)
 
 if __name__ == "__main__":
     main()
