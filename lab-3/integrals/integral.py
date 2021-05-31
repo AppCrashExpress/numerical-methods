ef20352297edbb83716b05a3feb39b0af2f97a15
AppCrashExpress <efimov.s2000@gmail.com>
2021-05-31 18:15:16 +0300

    Improve printing in integral methods

--- lab-3/integrals/integral.py
+++ lab-3/integrals/integral.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 def f(x):
-    # return 1 / ( (3 * x + 4) * x + 2 )
-    return x / (3*x + 4) ** 2
+    return 1 / ( (3 * x + 4) * x + 2 )
 
 def solve_rectangle(f, x, h):
     return h * ( sum([f( (x[i] + x[i+1]) / 2 ) for i in range(len(x) - 1)]) )
@@ -24,6 +23,10 @@ def test_rrr(rect, trape, simps):
     }
 
 def main():
+    def print_pairwise(xs, ys):
+        for i, (x, y) in enumerate(zip(xs, ys)):
+            print(f"{i}: {x:.3f} {y}")
+
     h = [0.5, 0.25]
     x0, xn = -1, 1
 
@@ -32,18 +35,27 @@ def main():
     simps = []
 
     for h_i in h:
+        print(f"For step {h_i}:")
+
         x = list(np.arange(x0, xn + h_i, h_i))
         y = [f(xi) for xi in x]
+        print("Points")
+        print_pairwise(x, y)
+
         rect.append(solve_rectangle(f, x, h_i))
         trape.append(solve_trapezoid(y, h_i))
         simps.append(solve_simpson(y, h_i))
 
+        print(f"Rectangular method: {rect[-1]}")
+        print(f"Trapezoid   method: {trape[-1]}")
+        print(f"Simpson     method: {simps[-1]}")
+        print()
+
     error = test_rrr(rect, trape, simps)
 
-    print(rect)
-    print(trape)
-    print(simps)
-    print(error)
+    print(f"Rectangular error: {error['rectangle']}")
+    print(f"Trapezoid   error: {error['trapezoid']}")
+    print(f"Simpson     error: {error['simpson']}")
 
 if __name__ == "__main__":
     main()
